c714fc0b0778c972b72d047ae2832d083b8a65d9
RicJM <116638687+Rimcant@users.noreply.github.com>
2023-01-04 09:39:19 +0000

    04-01-2023

--- webdev/CalculadoraServerSiDe/calculator.js
+++ webdev/CalculadoraServerSiDe/calculator.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 
-const app = express()
+const app = express();
+app.use(bodyParser.urlencoded({ extendend: true}));
 
 app.get("/", function (req, res) {
-    res.send("<h1>hello world </h1>");   
+    res.sendFile(__dirname + "/index.html"); 
+    
+})
+app.post("/", function (req, res) {
+    console.log(req.body); 
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("O resultado é " + result)
+})
 
-    
-     
-    
-    })
 app.listen(3000, function () {
     console.log("server started on port 3000")
 });
