08f933d226d13cb133688ce7525d0e2c6b3e3b45
RicJM <116638687+Rimcant@users.noreply.github.com>
2023-02-15 09:55:01 +0000

    15-02-23

---
+++ webdev/028_react_001/my-app/src/FoodList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FoodList = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h2>Comida Favorita</h2>
+      <ul>
+        <li>Pizza</li>
+        <li>Hambúrguer</li>
+        <li>Sushi</li>
+        <li>Salada</li>
+      </ul>
+    </div>
+  );
+};
+
+export default FoodList;
