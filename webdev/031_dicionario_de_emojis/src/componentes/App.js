a3d1ece978fb6e5999c6d49c96819aa6c088b83b
RicJM <116638687+Rimcant@users.noreply.github.com>
2023-02-24 10:59:41 +0000

    24-02-23

--- webdev/031_dicionario_de_emojis/src/componentes/App.js
+++ webdev/031_dicionario_de_emojis/src/componentes/App.js
@@ -2,21 +2,40 @@ import React from "react";
 import CartaoEmoji from './cartaoemoji'
 import emojipedia from './emojipedia'
 
+function criarCartaoEmoji(emoji) {
+  return <CartaoEmoji
+  key={emoji.id}
+  emoji={emoji.emoji}
+  name={emoji.name}
+  meaning={emoji.meaning} />
+}
+
 function App() {
   return (
     <div>
+
+      <h3>
+        ola
+      </h3>
+      <h2>
+        {emojipedia.filter(function (string) {
+          return string.meaning.length > 20
+        }).map(function (string) {
+        return <p>{string.meaning.substring(0,100)}</p>
+        })}
+      </h2>
       <h1>
         <span>emojipedia</span>
       </h1>
-      {emojipedia.map((emoji)=> (
-      <CartaoEmoji
-        key={emoji.id}
-        emoji={emoji.emoji}
-        name={emoji.name}
-        meaning={emoji.meaning}
-      />
-      
-      ))}
+      {emojipedia.map((emoji) =>
+        <CartaoEmoji
+          key={emoji.id}
+          emoji={emoji.emoji}
+          name={emoji.name}
+          meaning={emoji.meaning}
+          
+        />
+      )}
     </div>
   );
 }
